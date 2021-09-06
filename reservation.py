92c10602a093b44cfbeb770d241358cb4455ae22
Walnuts <ryamkun@gmail.com>
2021-09-07 07:51:27 +0900

    [update]セッションタイムアウトエラーを修正
    Timed out receiving message from renderer: 600.000

--- reservation.py
+++ reservation.py
@@ -258,6 +258,19 @@ def select_medical(driver):
 def reserve():
      
     options = Options()
+    options.add_argument("start-maximized")
+    options.add_argument("enable-automation")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-infobars")
+    options.add_argument('--disable-extensions')
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-browser-side-navigation")
+    options.add_argument("--disable-gpu")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    prefs = {"profile.default_content_setting_values.notifications" : 2}
+    options.add_experimental_option("prefs",prefs)
+
     if config["headless"]:
         # ヘッドレスモードでブラウザを起動
         options.add_argument('--headless')
